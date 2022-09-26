e039797024f3aecc390248b9f7acffe765a90581
Facundo Saporiti <facundo.saporiti@hotmail.com>
2022-09-25 23:04:42 -0300

    corrijo para deployar

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const publicPath = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));                //le dice a expres que la carpeta public es una carpeta estatica con archivos para consumir
 
-app.listen(process.env.PORT || 3080, () => {
-    console.log("Servidor corriendo en el puerto 3080")
-})
 
 app.get("/", function(req,res){
     res.sendFile(path.resolve(__dirname, "./views/index.html"));
@@ -23,3 +20,6 @@ app.get("/Views/register.html", (req,res) => {
     res.sendFile(path.resolve (__dirname, "./Views/register.html"))
 })
 
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Servidor corriendo en el puerto 3000")
+})
